daebb21aa89817797080808d01e4e4d32c2582e1
Mohamed Saad <techuniqueeg@gmail.com>
2020-09-07 02:57:46 +0200

    first commit

---
+++ database/migrations/2019_02_27_224902_create_orders_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+ 
+
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('client_id')->unsigned();
+            $table->double('total_price', 8, 2)->nullable();
+            $table->timestamps();
+            
+            $table->foreign('client_id')->references('id')->on('clients')->onDelete('cascade');
+        });
+    }
+
+    
+    
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
---
+++ database/migrations/2019_06_06_105828_create_product_order_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductOrderTable extends Migration
+{
+
+
+    public function up()
+    {
+        Schema::create('product_order', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('product_id')->unsigned();
+            $table->integer('order_id')->unsigned();
+            $table->integer('quantity')->default(1);
+
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+        });
+    }
+
+
+
+    public function down()
+    {
+        Schema::dropIfExists('product_order');
+    }
+}
